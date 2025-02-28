1501bd40385769c1219cff9d6ecd3580eb4ecf1c
1aman1 <amangupta@outlook.com>
2025-02-28 21:44:35 +0530

    +

--- datastructures/linkedlists/LC/83.Remove-Duplicates-from-Sorted-List.cpp
+++ datastructures/linkedlists/LC/83.Remove-Duplicates-from-Sorted-List.cpp
@@ -18,11 +18,13 @@ public:
     while (curr != nullptr && curr->next != nullptr)
     {
       if (curr->val == curr->next->val)
+      {
         curr->next = curr->next->next;
-    }
-    else
-    {
-      curr = curr->next;
+      }
+      else
+      {
+        curr = curr->next;
+      }
     }
 
     return head;
