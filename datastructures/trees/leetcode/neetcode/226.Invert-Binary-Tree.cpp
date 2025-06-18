dd605c509c73ddd7ff125bd4a67ed797ce088177
1aman1 <amangupta@outlook.com>
2025-06-18 11:44:00 +0530

    +

---
+++ datastructures/trees/leetcode/neetcode/226.Invert-Binary-Tree.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    TreeNode *invertTree(TreeNode *root)
+    {
+        if (root == nullptr)
+            return root;
+
+        swap(root->left, root->right);
+        root->left = invertTree(root->left);
+        root->right = invertTree(root->right);
+
+        return root;
+    }
+};
