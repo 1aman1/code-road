3e86b511288ed9805e9cbaee85aa36f4eccf1725
1aman1 <amangupta@outlook.com>
2025-07-14 10:54:17 +0530

    +

--- datastructures/linkedlists/leetcode/146.LRU-Cache.cpp
+++ datastructures/linkedlists/leetcode/146.LRU-Cache.cpp
@@ -14,9 +14,7 @@ class LRUCache
     std::unordered_map<int, std::list<Pair>::iterator> keyItrCache;
 
 public:
-    LRUCache(int capacity) : capacity(capacity)
-    {
-    }
+    LRUCache(int capacity) : capacity(capacity) {}
 
     int get(int key)
     {
@@ -24,7 +22,7 @@ public:
             return -1;
 
         const auto &itrToValue = keyItrCache.at(key);
-        kVcontainer.splice(begin(kVcontainer), kVcontainer, itrToValue);
+        kVcontainer.splice(kVcontainer.begin(), kVcontainer, itrToValue);
         return kVcontainer.front().value;
     }
 
@@ -33,7 +31,7 @@ public:
         if (keyItrCache.count(key))
         {
             const auto &itrToValue = keyItrCache.at(key);
-            kVcontainer.splice(begin(kVcontainer), kVcontainer, itrToValue);
+            kVcontainer.splice(kVcontainer.begin(), kVcontainer, itrToValue);
             itrToValue->value = value;
 
             return;
@@ -41,14 +39,11 @@ public:
 
         if (capacity == kVcontainer.size())
         {
-            // const auto &itrToValue = kVcontainer.back();
             keyItrCache.erase(kVcontainer.back().key);
             kVcontainer.pop_back();
         }
 
         kVcontainer.emplace_front(key, value);
-        keyItrCache[key] = begin(kVcontainer);
+        keyItrCache[key] = kVcontainer.begin();
     }
 };
-
-// is this O(1) operations for get and put?
