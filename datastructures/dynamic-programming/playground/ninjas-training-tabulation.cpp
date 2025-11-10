82a26a30012319450f23e70241c37c017f989111
1aman1 <amangupta@outlook.com>
2025-11-10 10:29:01 +0530

    .

--- datastructures/dynamic-programming/playground/ninjas-training-tabulation.cpp
+++ datastructures/dynamic-programming/playground/ninjas-training-tabulation.cpp
@@ -3,39 +3,40 @@ using namespace std;
 
 int ninjaTraining(int n, vector<vector<int>> &points)
 {
-    vector<vector<int>> cache(n, vector<int>(4, 0));
+    vector<vector<int>> cache(n, vector<int>(3, 0));
 
     cache[0][0] = max(points[0][1], points[0][2]);
     cache[0][1] = max(points[0][0], points[0][2]);
     cache[0][2] = max(points[0][0], points[0][1]);
-    cache[0][3] = max(points[0][0], max(points[0][1], points[0][2]));
 
     for (int day = 1; day < n; day++)
     {
-        for (int last = 3; last >= 0; last--)
+        for (int lastActivity = 0; lastActivity < 3; lastActivity++)
         {
-            cache[day][last] = 0;
-            for (int activity = 0; activity <= 2; activity++)
+            cache[day][lastActivity] = 0;
+            
+            for (int task = 0; task < 3; task++)
             {
-                if (activity != last)
+                if (task != lastActivity)
                 {
-                    cache[day][last] = max(cache[day][last],
-                                           points[day][activity] + cache[day - 1][activity]);
+                    cache[day][lastActivity] = max(cache[day][lastActivity],
+                                                   points[day][task] + cache[day - 1][task]);
                 }
             }
         }
     }
 
-    return cache[n - 1][3];
+    return max({cache[n - 1][0], cache[n - 1][1], cache[n - 1][2]});
 }
 
 int main()
 {
-    // Define the points matrix
-    vector<vector<int>> points = {{10, 40, 70},
-                                  {20, 50, 80},
-                                  {30, 60, 90}};
-    int n = points.size(); // Get the number of days
-    // Call the ninjaTraining function to find the maximum points and print the result
+    vector<vector<int>> points = {
+        {10, 40, 70},
+        {20, 50, 80},
+        {30, 60, 90}};
+
+    int n = points.size();
     cout << ninjaTraining(n, points);
+    return 0;
 }
